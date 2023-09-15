6cc9edbd20ba8335f66e1f0192ae63946130a53a
Conrado-Djugo <yuri84rn@gmail.com>
2023-09-15 12:00:53 -0300

    Edit Script

--- js/script.js
+++ js/script.js
@@ -1,25 +1,37 @@
-
+const text = document.getElementById('textp');
+const lampOnOff = document.getElementById('lamp');
 const result= document.getElementById('num')
 let pointAdd = false;
+let onOff = false;
 
-function insert (value){
+
+function insert(value) {
     result.innerHTML += value;
-    pointAdd = false
-
+    pointAdd = false;
+    text.textContent = "ON";
+    lampOnOff.classList.add('ativo')
+    
 }
+
 
 function deletAll(){
     result.innerHTML = '';
     pointAdd = false
+    text.textContent = "OFF";
+    lampOnOff.classList.remove('ativo')
 }
 
-function backNum(){
-    if( result.textContent) {
-        const displayNum = document.getElementById('num').innerHTML
-        result.innerHTML = displayNum.substring(0, displayNum.length -1)
-       
+function backNum() {
+    const displayNum = document.getElementById('num').innerHTML;
+    const updatedDisplayNum = displayNum.substring(0, displayNum.length - 1);
+    result.innerHTML = updatedDisplayNum;
+
+    if (!/[0-9a-zA-Z]/.test(updatedDisplayNum)) {
+        text.textContent = "OFF";
+        lampOnOff.classList.remove('ativo');
     }
 }
+
 
 function confirm(){
     if(result.textContent != 'Erro'){
@@ -33,6 +45,8 @@ function addNum(){
         result.innerHTML += '.';
         pointAdd = true
     }
- 
+}
 
+function buttonOnOff(){
+  
 }
